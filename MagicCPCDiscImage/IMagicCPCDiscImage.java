0810836f72db672d6bc0b95b65b36c0cc705cdf5
Renaud Helias <renaudhelias@gmail.com>
2018-08-05 17:26:00 +0200

    MagicCPCDisc

---
+++ MagicCPCDiscImage/IMagicCPCDiscImage.java
@@ -0,0 +1,28 @@
+String[] getDir(){
+....
+}
+
+byte[] getFile(String filename){
+...
+}
+void putFile(String filename, String filenameOnDSK){
+...
+]
+int getSize(String filename){
+}
+int getDiskSize(){
+}
+possible?
+For several formats... DATA 40, DATA 80, ROMDOS formats etc
+void setUser(String filename, int user){
+}
+void protectFile(String filename, boolean protect){
+...
+}
+void systemFile(String filename, boolean system){
+...
+}
+
+//system = hidden file
+//CPCXFS does the same... But I only have C source (And I dont understand any)
+// http://cpctech.cpc-live.com/download/cpcxfs.zip
